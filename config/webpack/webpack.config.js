ae4e813a16d82cff89e09fd928727740605b0926
Chase Adams <cadams@walmartlabs.com>
2015-08-25 22:28:32 -0700

    Updates bolt to use babel stage 2 instead of 0

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: [/node_modules/],
-      loader: "babel-loader?stage=0"
+      loader: "babel-loader?stage=2"
     }, {
       test: /\.css$/,
       loader: "style-loader!css-loader"
