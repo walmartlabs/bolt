00b5a5ecff13f123856b2c2b7c616584ec4008ac
Chase Adams <cadams@walmartlabs.com>
2015-09-30 11:07:53 -0700

    Moves spec tests, adds istanbul ignore for some hard to test things

--- test/spec/lib/logger.spec.js
+++ test/spec/lib/logger.spec.js
@@ -11,24 +11,16 @@ describe("logger", function () {
   });
 
   afterEach(function () {
-    process.env.NODE_ENV = NODE_ENVRestore;
     sandbox.restore();
   });
 
-  it("should not call console log if NODE_ENV is 'test'", function () {
-    process.env.NODE_ENV = "test";
-    spy = sandbox.spy(console, "log");
-
-    logger("hello", "world");
-    expect(spy).to.not.have.been.called;
-  });
-
   it("should call console log if NODE_ENV is not 'test'", function () {
-    process.env.NODE_ENV = "dev";
+    process.env.TEST_LOGGER = true;
     stub = sandbox.stub(global.console, "log");
 
     logger("hello", "world");
     expect(stub).to.have.been.called;
     global.console.log.restore();
+    delete process.env.TEST_LOGGER;
   });
 });
