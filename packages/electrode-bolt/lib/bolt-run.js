b9bac9964e6c62c1278e0495013949709867c95c
Chase Adams <cadams@walmartlabs.com>
2015-10-22 15:15:33 -0700

    Remove bolt/lib from version control

--- packages/electrode-bolt/lib/bolt-run.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var Bolt = require("./bolt");
-
-var bolt = new Bolt(process);
-bolt.run();
--- packages/electrode-bolt/lib/logger.js
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore next */
-"use strict";
-
-module.exports = function () {
-  if (process.env.NODE_ENV === "test" && !process.env.TEST_LOGGER) return;
-
-  var args = Array.prototype.slice.call(arguments);
-  console.log.apply(console, args);
-};
